208348b3af01f9a0627d98b9511191375a2d11c0
legui04 <martin.leguizamon@outlook.es>
2021-03-16 20:15:10 -0300

    poto

--- src/main/java/com/ufranco/java2048/frontend/AlertBox.java
+++ src/main/java/com/ufranco/java2048/frontend/AlertBox.java
@@ -7,7 +7,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-public class AlertBox {
+public class AlertBox { //jebus es la luz
   public static void display(String title, String message) {
     Stage window = new Stage();
 
