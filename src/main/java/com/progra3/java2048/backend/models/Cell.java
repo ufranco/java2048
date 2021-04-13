205be3de0c1b988d68bde0f8b3f338a46b900168
vFlorentin <victorsantiagoflorentin@gmail.com>
2021-04-13 00:57:21 -0300

    clean branch

--- src/main/java/com/progra3/java2048/backend/models/Cell.java
+++
@@ -1,4 +0,0 @@
-package com.progra3.java2048.backend.models;
-
-public record Cell(int x, int y) {}
-
--- src/main/java/com/progra3/java2048/backend/utils/Movement.java
+++
@@ -1,8 +0,0 @@
-package com.progra3.java2048.backend.utils;
-
-public enum Movement {
-  UP,
-  DOWN,
-  LEFT,
-  RIGHT
-}
